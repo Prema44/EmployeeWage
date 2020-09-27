5a71734495b310061020a670d1a3307399905ece
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-27 19:26:29 +0530

    UC3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,11 +4,12 @@ public class EmployeeWage {
 	public static void main(String[] args)
 	{
 		int is_full_time=1;
+		int is_part_time=2;
 		int emp_rate_per_hour=20;
 		int emp_wage=0;
 		int emp_hours=0;
-		int empcheck=(int) Math.floor(Math.random()*10)%2;
-		if(empcheck==is_full_time)
+		int empcheck=(int) Math.floor(Math.random()*10)%3;
+		if(empcheck==is_full_time || empcheck==is_part_time)
 		{
 			System.out.println("Employee is present");
 		}
@@ -20,6 +21,11 @@ public class EmployeeWage {
 		{
 			emp_hours=8;
 		}
+		else if (empcheck==is_part_time)
+		{
+			emp_hours=4;
+		}
+		
 		emp_wage=emp_hours*emp_rate_per_hour;
 		System.out.println("Emp Wage: "+emp_wage);
 	}
