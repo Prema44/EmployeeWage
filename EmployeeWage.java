38458edf60261c6fbd67383f3d7c1bb941c72f16
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-27 19:08:58 +0530

    UC2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,6 +4,9 @@ public class EmployeeWage {
 	public static void main(String[] args)
 	{
 		int is_full_time=1;
+		int emp_rate_per_hour=20;
+		int emp_wage=0;
+		int emp_hours=0;
 		int empcheck=(int) Math.floor(Math.random()*10)%2;
 		if(empcheck==is_full_time)
 		{
@@ -13,6 +16,12 @@ public class EmployeeWage {
 		{
 			System.out.println("employee is absent");
 		}
+		if(empcheck==is_full_time)
+		{
+			emp_hours=8;
+		}
+		emp_wage=emp_hours*emp_rate_per_hour;
+		System.out.println("Emp Wage: "+emp_wage);
 	}
 
 }
