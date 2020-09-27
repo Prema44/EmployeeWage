7d52031c8ba76112439ed66e008975b9bc90201a
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-27 20:00:26 +0530

    UC5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,21 +8,27 @@ public class EmployeeWage {
 		int emp_rate_per_hour=20;
 		int emp_wage=0;
 		int emp_hours=0;
-		int empcheck=(int) Math.floor(Math.random()*10)%3;
-		switch(empcheck)
+		int num_of_working_days=20;
+		int totalempwage=0;
+		for(int day=0;day<num_of_working_days;day++)
 		{
-		case 1:
-			emp_hours=8;
-			break;
-		case 2:
-			emp_hours=4;
-			break;
-		default:
-			emp_hours=0;
-		}
-		
+			int empcheck=(int) Math.floor(Math.random()*10)%3;
+			switch(empcheck)
+			{
+			case 1:
+				emp_hours=8;
+				break;
+			case 2:
+				emp_hours=4;
+				break;
+			default:
+				emp_hours=0;
+			}
+		totalempwage+=emp_wage;	
 		emp_wage=emp_hours*emp_rate_per_hour;
 		System.out.println("Emp Wage: "+emp_wage);
+		}
+		System.out.println("Total emp wage :"+ totalempwage);
 	}
 
 }
