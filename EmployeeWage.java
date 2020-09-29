da53af7515622114093516f35a76d9a59b8b68d0
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-29 13:41:58 +0530

    Employee Wage using ArrayList

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,47 +1,33 @@
 package employeewage;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.ArrayList;
 
-public class EmployeeWage implements IComputeEmpWage{
+public class EmployeeWage{
 	
 		public static final int is_part_time = 1;
 		public static final int is_full_time = 2;
 		
 		private int numOfCompany = 0;
-		
-		private LinkedList<CompanyEmpWage> companyEmpWageList ;
-		private Map<String , CompanyEmpWage> companyToEmpWageMap;
+		private ArrayList<CompanyEmpWage> companyEmpWageArrayList;
 		
 		public EmployeeWage()
 		{
-			companyEmpWageList = new LinkedList<>();
-			companyToEmpWageMap = new HashMap<>();
-			
+			companyEmpWageArrayList = new ArrayList<CompanyEmpWage>();
 		}
 		
-		public void addCompanyEmpWage(String company, int emp_rate_hour, int no_of_working_days, int max_hr_in_month)
+		private void addCompanyEmpWage(String company, int emp_rate_hour, int no_of_working_days, int max_hr_in_month)
 		{
-			CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, emp_rate_hour, no_of_working_days, max_hr_in_month);
-			companyEmpWageList.add( companyEmpWage);
-			companyToEmpWageMap.put(company, companyEmpWage);
-			
+			companyEmpWageArrayList.add(new CompanyEmpWage(company, emp_rate_hour, no_of_working_days, max_hr_in_month));
+			numOfCompany++;
 		}
 		
-		public void computeEmpWage()
+		private void computeEmpWage()
 		{
-			for(int i = 0; i < companyEmpWageList.size(); i++)
+			for(int i = 0; i < numOfCompany; i++)
 			{
-				CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
-				companyEmpWage.setTotalEmpWage( this.computeEmpWage(companyEmpWage));
-				System.out.println(companyEmpWage);
+				companyEmpWageArrayList.get(i).setTotalEmpWage( this.computeEmpWage ( companyEmpWageArrayList.get(i)));
+				System.out.println(companyEmpWageArrayList.get(i));
 			}
-		}
-		
-		public int getTotalWage( String company)
-		{
-			return companyToEmpWageMap.get(company).totalEmpWage;
 		}
 		
 		private int computeEmpWage( CompanyEmpWage companyEmpWage)
@@ -71,7 +57,7 @@ public class EmployeeWage implements IComputeEmpWage{
 		
 		public static void main(String[] args) {
 			
-			IComputeEmpWage empWageBuilder = new EmployeeWage();
+			EmployeeWage empWageBuilder = new EmployeeWage();
 			empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
 			empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
 			empWageBuilder.computeEmpWage();
